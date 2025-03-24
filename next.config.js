3bf876f2643aa97a7f8cc16e9b72b425929df338
com.memo <mrt.t0304@gmail.com>
2025-03-24 17:58:21 +0900

    型定義エラーを修正: ThemeProviderの型を自前で定義、Next.js設定を単純化

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@
 const nextConfig = {
   output: 'export',
   // basePath: '/portfolio', // リポジトリ名がportfolioの場合
-  images: {
-    unoptimized: true,
-  },
 };
 
 module.exports = nextConfig; 
--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -2,7 +2,15 @@
 
 import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-import { type ThemeProviderProps } from "next-themes/dist/types";
+
+type ThemeProviderProps = {
+  children: React.ReactNode;
+  attribute?: string;
+  defaultTheme?: string;
+  enableSystem?: boolean;
+  disableTransitionOnChange?: boolean;
+  [key: string]: any;
+};
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
