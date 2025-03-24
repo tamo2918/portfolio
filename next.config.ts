b65950732ea0451aa16b718c8eaedc58d2fa1fd9
com.memo <mrt.t0304@gmail.com>
2025-03-24 17:36:25 +0900

    GitHub Pages用の設定を追加

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  // basePath: '/portfolio', // リポジトリ名がportfolioの場合
   images: {
     domains: ['assets.st-note.com'],
     remotePatterns: [
